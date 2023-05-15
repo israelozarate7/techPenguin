60a6b8a8389ed35b956cb74934482ca66847423d
Israel Ortiz de Zarate <israelortizdezarate@MacBook-Pro-de-Israel.local>
2023-05-15 16:54:25 +0200

    Create RG

--- main.tf
+++ main.tf
@@ -2,31 +2,3 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = var.resource_group_name
 }
-
-resource "azurerm_storage_account" "example" {
-  name                     = "techpenguinsa"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = var.resource_group_location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_app_service_plan" "example" {
-  name                = "techpenguinappsp"
-  location            = var.resource_group_location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-
-resource "azurerm_function_app" "example" {
-  name                       = "techpenguinaf"
-  location                   = var.resource_group_location
-  resource_group_name        = azurerm_resource_group.rg.name
-  app_service_plan_id        = azurerm_app_service_plan.example.id
-  storage_account_name       = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
-}
